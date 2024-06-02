57a83616e94b9bb4958286225243898d3a8b4ad4
santosh <sahsantosh257@gmail.com>
2024-06-02 22:58:34 +0545

    Sklearn Iris dataset model.

--- chapter_3/iris_dataset.py
+++ chapter_3/iris_dataset.py
@@ -41,6 +41,72 @@ print("Accuracy Score:  %.3f" % accuracy_score(y_test, y_pred))
 
 print("Accuracy : %.3f" % ppn.score(X_test_std, y_test))
 
+# Plot decision regions 
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+
+
+def plot_decision_regions(X, y, classifier, test_index=None, resolution = 0.02):
+
+    # 3 Setup marker , generator, and Colormap
+    markers = ('o', 's','^', 'v', ">")
+    colors = ('red','blue', 'lightgreen', "grey", "cyan")
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # plot the decision surface
+    x1_min, x1_max = X[:,0].min()-1, X[:,0].max()+1
+    x2_min, x2_max = X[:,1].min()-1, X[:,1].max()+1
+    # x3_min, x3_max = X[:,2].min()-1, X[:,2].max()+1
+    # x4_min, x4_max = X[:,3].min()-1, X[:,3].max()+1
+
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                            np.arange(x2_min, x2_max, resolution),
+                            # np.arange(x3_min, x3_max, resolution),
+                            # np.arange(x4_min, x4_max, resolution)
+                            )
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    #plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0],
+                    y = X[y == cl, 1],
+                    alpha=0.8,
+                    c = colors[idx],
+                    marker= markers[idx],
+                    label = f'Class {cl}',
+                    edgecolors='black')
+    # highlight test examples
+    if test_index:
+        # plot all examples
+        X_test, y_test = X[test_index, :], y[test_index]
+        plt.scatter(X_test[:, 0], X_test[:, 1],
+        c = "none", 
+        edgecolors="black",
+        alpha=1.0,
+        linewidth = 1,
+        marker='o',
+        s=100,
+        label="Test set")
+
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std,
+                    y=y_combined,
+                    classifier=ppn,
+                    test_index=range(105,150))
+
+plt.xlabel("Petal Length [standardarized]")
+plt.ylabel("Petal Width [standarized]")
+plt.legend(loc= 'upper left')
+plt.tight_layout()
+plt.show()
+
 
 
 
